5871eee6a66013b600a3e5967b121124813f5413
detergentishere <suryawanshiisha143@gmail.com>
2025-08-12 18:07:01 +0530

    Update ImpactPage.jsx
    security purposes

--- Frontend/src/pages/ImpactPage.jsx
+++ Frontend/src/pages/ImpactPage.jsx
@@ -94,7 +94,7 @@ Provide numbers only, and a short explanation.
 `;
 
         const response = await fetch(
-          `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateText?key=AIzaSyAhh3AV0HWEB_RjvhfGIUZXsVBfdq5gGHo`, // <-- Replace here
+          `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateText?key=`, // <-- Replace here
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
